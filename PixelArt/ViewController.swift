349554b1b4429d78dddfad66f173b139529e3889
Anurag Pandit <codeanuragpandit@gmail.com>
2024-09-29 18:09:13 +0530

    Initial Commit

---
+++ PixelArt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PixelArt
+//
+//  Created by Anurag Pandit on 29/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PixelArtUITests/PixelArtUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PixelArtUITestsLaunchTests.swift
+//  PixelArtUITests
+//
+//  Created by Anurag Pandit on 29/09/24.
+//
+
+import XCTest
+
+final class PixelArtUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
